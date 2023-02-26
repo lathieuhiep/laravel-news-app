17a4fe1283f81d5973e225d6b11bd48f7f0cbf80
LaThieuHiep <khacdiepkma90@gmail.com>
2023-02-27 00:17:44 +0700

    update code

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -9,9 +9,18 @@
             @csrf
             <div class="post-body row">
                 <div class="post-body__content col-12 col-md-8">
-                    <div class="form-group">
+                    <div class="form-group mb-5">
                         <input type="text" id="title" class="form-control" name="post_title"
                                placeholder="{{ __('Add title') }}" autocomplete="off" aria-label="">
+                    </div>
+
+                    <div class="form-group mb-5">
+                        <textarea id="editor-content" class="form-control" aria-label="" name="content"></textarea>
+                    </div>
+
+                    <div class="form-group mb-5">
+                        <label for="excerpt" class="mb-3">Excerpt</label>
+                        <textarea id="excerpt" class="form-control" aria-label="" name="excerpt"></textarea>
                     </div>
                 </div>
 
@@ -19,4 +28,9 @@
             </div>
         </form>
     </div>
+@endsection
+
+@section('scripts')
+<script src="{{ asset('admin/assets/js/tinymce/tinymce.min.js') }}"></script>
+<script src="{{ asset('admin/assets/js/posts.js') }}"></script>
 @endsection
