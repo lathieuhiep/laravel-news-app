cd661c6abaa236d1e65d406358e0105e6a82261a
lakhacdiep <diep.lk@beetechsoft.com>
2023-03-13 15:24:25 +0700

    update code

---
+++ database/migrations/2023_03_01_045955_add_media_id_to_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+	        $table->foreignIdFor(Media::class, 'media_id')->after('excerpt')->nullable()->constrained('media')->onDelete('NO ACTION');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+	        $table->dropColumn('media_id');
+        });
+    }
+};
